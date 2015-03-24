5d0d5ac7543868e0af39f857409ce367a822ea74
Tollef Fog Heen <tfheen@fastly.com>
2015-03-24 11:46:42 +0100

    Expose parent too in the search_graph view

--- doc/views.sql
+++ doc/views.sql
@@ -1,11 +1,11 @@
 CREATE OR REPLACE VIEW secret_tree AS WITH RECURSIVE
-search_graph(secret_id, path, value) AS
+search_graph(secret_id, parent, path, key, value) AS
 (
-SELECT  secret_id, ARRAY[key], value
+SELECT  secret_id, parent, ARRAY[key], key, value
 FROM    secrets
 WHERE parent IS NULL
 UNION ALL
-SELECT  s.secret_id, sg.path|| s.key, s.value
+SELECT  s.secret_id, s.parent, sg.path|| s.key, s.key, s.value
 FROM    search_graph sg
 JOIN    secrets s
 ON      s.parent = sg.secret_id
